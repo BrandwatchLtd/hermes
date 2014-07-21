9522a523f906fbda6856416ef5337f5a0925e3b7
Mark Everitt <marke@brandwatch.com>
2014-07-21 17:27:41 +0100

    Adds hermes.js and minified counterpart.

---
+++ hermes.js
@@ -0,0 +1,202 @@
+/**
+ * Each animation style is composed of three parts:
+ *
+ *  - in: A css class to animate/transition the element in.
+ *  - paused: A css class to use when the message is static.
+ *  - out: A css class to animate/transition the element out.
+ *
+ * The options object includes those fields, and a pauseTime field in ms.
+ *
+ * It is important that the in animation/transition ends with the same style that will be used for
+ * paused, and that the out animation/transition starts with the same style too in order to make the
+ * steps seamless.
+ */
+
+// IIFE to protect global scope.
+(function loadHermes() {
+    function makeHermes($) {
+        // Events that may be triggered when an animation or transition completes.
+        var endEvents = [
+            'animationend',
+            'webkitAnimationEnd',
+            'oAnimationEnd',
+            'MSAnimationEnd',
+            'transitionend',
+            'webkitTransitionEnd',
+            'oTransitionEnd',
+            'MSTransitionEnd'
+        ].join(' ');
+
+        function makeNotification(style, message) {
+            var $li = $(document.createElement('li'));
+
+            if (style.shared) {
+                $li.addClass(style.shared);
+            }
+
+            if (typeof message === 'string') {
+                $li.html(message);
+            } else {
+                $li.append($(message));
+            }
+
+            // Used for the pause step.
+            var pauseTimeout = null;
+            var afterEnd = null;
+            var state = 'before-start';
+
+            function animateIn(callback) {
+                state = 'animating-in';
+
+                $li.addClass(style.in);
+
+                $li.on(endEvents, function (event) {
+                    event.stopPropagation();
+
+                    $li.off();
+                    $li.removeClass(style.in);
+
+                    callback();
+                });
+            }
+
+            function pause(callback) {
+                state = 'paused';
+
+                $li.addClass(style.paused || '');
+
+                pauseTimeout = setTimeout(function () {
+                    pauseTimeout = null;
+
+                    $li.removeClass(style.paused || '');
+
+                    callback();
+                }, style.pauseTime || 3000);
+            }
+
+            function animateOut() {
+                state = 'animating-out';
+
+                $li.addClass(style.out);
+
+                $li.on(endEvents, function (event) {
+                    event.stopPropagation();
+
+                    $li.remove();
+
+                    afterEnd();
+                });
+            }
+
+            // The callback to the in animation should trigger a pause, and then animate out.
+            var inCallback = function () {
+                pause(animateOut);
+            };
+
+            return {
+                start: function (callback) {
+                    afterEnd = callback;
+
+                    animateIn(function () {
+                        inCallback();
+                    });
+                },
+                cancel: function () {
+                    if (state === 'animating-in') {
+                        // Skip the pause step.
+                        inCallback = animateOut;
+                    }
+
+                    if (state === 'paused') {
+                        // Clear the pause.
+                        clearTimeout(pauseTimeout);
+
+                        // Begin out animation.
+                        animateOut();
+                    }
+                },
+                $li: $li
+            };
+        }
+
+
+        // Configure and return a notifier object.
+        function hermes(options) {
+            var $ul = $(document.createElement('ul'));
+            var types = Object.keys(options.styles);
+
+            // If no maxNotifications was given, there is no maximum.
+            var maxNotifications = options.maxNotifications || Infinity;
+            var notifications = [];
+
+            if (options.listClasses) {
+                $ul.addClass(options.listClasses);
+            }
+
+            $ul.appendTo($(options.container));
+
+            // Append a notification with the given message and style to a list. Expire oldest
+            // notification if the list has grown too large.
+            function appendNotification(style, message) {
+                var notification = makeNotification(style, message);
+
+                notifications.push(notification);
+
+                // If we've gone over the max notifications, animate oldest out early.
+                if (notifications.length > maxNotifications) {
+                    notifications.shift().cancel();
+                }
+
+                notification.start(function notificationComplete() {
+                    var index = notifications.indexOf(notification);
+
+                    // If this notification animated out naturally, it needs to be removed from the
+                    // notifications array.
+                    if (index !== -1) {
+                        notifications.splice(index, 1);
+                    }
+                });
+
+                $ul.prepend(notification.$li);
+            }
+
+            // Append a new notification type to a notifier object.
+            function makeType(notifier, type) {
+                var style = options.styles[type];
+
+                notifier[type] = function (message) {
+                    appendNotification(style, message);
+                };
+
+                return notifier;
+            }
+
+            // Populate an object with notification methods and return it.
+            return types.reduce(makeType, {});
+        }
+
+        return hermes;
+    }
+
+    // If hermes is being loaded with an AMD module loader.
+    if (typeof define === 'function' && define.amd) {
+        return define(['jquery'], makeHermes);
+    }
+
+    // If hermes is being loaded in Node.js or with Browserify.
+    if (typeof module === 'object' && module && module.exports) {
+        var $ = require('jquery');
+
+        module.exports = makeHermes($);
+
+        return;
+    }
+
+    // Finally, if the module is being loaded as a global, check that jQuery has been loaded, then
+    // append hermes to the window.
+    if (!window.jQuery) {
+        throw new Error('Hermes requires jQuery.');
+    }
+
+    window.hermes = makeHermes(window.jQuery);
+}());
